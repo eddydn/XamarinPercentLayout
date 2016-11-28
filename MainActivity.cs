ea49d2e4037a1f6e7109395694c1823ce6b95879
eddydn <eddydn@gmail.com>
2016-11-28 07:43:44 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinPercentLayout
+{
+    [Activity(Label = "XamarinPercentLayout", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+            RequestedOrientation = Android.Content.PM.ScreenOrientation.Landscape;
+        }
+    }
+}
+
